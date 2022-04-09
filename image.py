d4be60eb31ee6a791782e143c1371873f6c5fb79
Eden Lee <edenlee@Edens-MacBook-Pro.local>
2022-04-09 12:57:17 +0700

    fast commit

--- image.py
+++ image.py
@@ -1,9 +1,10 @@
 import argparse
+from PIL import Image
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--value-1', nargs=1)
-parser.add_argument('--value-2', nargs=1)
-parser.add_argument('email', nargs='+')
+parser.add_argument('--path', nargs=1)
+parser.add_argument('--file-name', nargs=1)
 args = parser.parse_args()
 
-print(args)
+im = Image.open(args.path[0] + '/' + args.file_name[0] + ".webp").convert('RGBA').convert('P', palette=Image.ADAPTIVE, colors=255)
+im.save(args.path[0] + '/' + args.file_name[0] + ".png", "png")
